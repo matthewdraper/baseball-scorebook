63fc41997a22af193cdfac4265fa3e4334794dbc
Matthew Draper <Matthew Draper>
2016-07-10 12:30:41 -0500

    feat: Added an main menu activity
    Fragments in use in the main activity. Add button color to the color.xml for the UI.

--- BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/MainActivity.java
+++ BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/MainActivity.java
@@ -3,11 +3,13 @@ package com.duxnort.baseballscorebook;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainMenuFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
