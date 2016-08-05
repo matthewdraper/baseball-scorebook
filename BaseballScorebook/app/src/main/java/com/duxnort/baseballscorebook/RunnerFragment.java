5dd1f58ea432abdd34bcf23edb407184b97a3c39
Matthew Draper <matt_draper@outlook.com>
2016-08-05 12:28:23 -0500

    Working on wiring all the buttons with the pressed button images.

--- BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/RunnerFragment.java
+++ BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/RunnerFragment.java
@@ -6,8 +6,11 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class RunnerFragment extends Fragment {
+
+    private Button btnOut, btnScored, btnAdvanced, btnStolenBase, btnCaughtStealing, btnUndo;
 
     private OnFragmentInteractionListener mListener;
 
@@ -24,7 +27,77 @@ public class RunnerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_runner, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_runner, container, false);
+        initializeButtons(rootView);
+        return rootView;
+    }
+
+    private void initializeButtons(View rootView) {
+        btnOut = (Button) rootView.findViewById(R.id.btnOut);
+        btnScored = (Button) rootView.findViewById(R.id.btnScored);
+        btnAdvanced = (Button) rootView.findViewById(R.id.btnAdvanced);
+        btnStolenBase = (Button) rootView.findViewById(R.id.btnStolenBase);
+        btnCaughtStealing = (Button) rootView.findViewById(R.id.btnCaughtStealing);
+        btnUndo = (Button) rootView.findViewById(R.id.btnUndo);
+
+        initializeOnTouchListeners();
+        initializeOnClickListeners();
+    }
+
+    private void initializeOnClickListeners() {
+
+    }
+
+    private void initializeOnTouchListeners() {
+
+    }
+
+    private void btnOutOnClick() {
+
+    }
+
+    private void btnOutOnTouch() {
+
+    }
+
+    private void btnScoredOnClick() {
+
+    }
+
+    private void btnScoredOnTouch() {
+
+    }
+
+    private void btnAdvancedOnClick() {
+
+    }
+
+    private void btnAdvancedOnTouch() {
+
+    }
+
+    private void btnStolenBaseOnClick() {
+
+    }
+
+    private void btnStolenBaseOnTouch() {
+
+    }
+
+    private void btnCaughtStealingOnClick() {
+
+    }
+
+    private void btnCaughtStealingOnTouch() {
+
+    }
+
+    private void btnUndoOnClick() {
+
+    }
+
+    private void btnUndoOnTouch() {
+
     }
 
     @Override
