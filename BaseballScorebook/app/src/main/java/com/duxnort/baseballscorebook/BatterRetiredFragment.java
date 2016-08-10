f59ca976645c1fd56fc35178e26a1c9924c9d921
Matthew Draper <matt_draper@outlook.com>
2016-08-10 08:35:40 -0500

    Finished framework for the wiring of the buttons for the BatterRetiredFragment and wired the OnTouchListeners.

--- BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/BatterRetiredFragment.java
+++ BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/BatterRetiredFragment.java
@@ -3,67 +3,35 @@ package com.duxnort.baseballscorebook;
 import android.app.Fragment;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BatterRetiredFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BatterRetiredFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BatterRetiredFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    private Button btnFoulOut, btnFlyOut, btnSwinging, btnLooking, btnGroundOut, btnLineOut,
+            btnUnassisted, btnSacFly, btnSacBunt, btnDoublePlay, btnTriplePlay;
 
     public BatterRetiredFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BatterRetiredFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BatterRetiredFragment newInstance(String param1, String param2) {
-        BatterRetiredFragment fragment = new BatterRetiredFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_batter_retired, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_batter_retired, container, false);
+        initializeButtons(rootView);
+        return rootView;
     }
 
     @Override
@@ -83,16 +51,304 @@ public class BatterRetiredFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+    private void initializeButtons(View rootView) {
+        btnFoulOut = (Button) rootView.findViewById(R.id.btnFoulOut);
+        btnFlyOut = (Button) rootView.findViewById(R.id.btnFlyOut);
+        btnSwinging = (Button) rootView.findViewById(R.id.btnSwinging);
+        btnLooking = (Button) rootView.findViewById(R.id.btnLooking);
+        btnGroundOut = (Button) rootView.findViewById(R.id.btnGroundOut);
+        btnLineOut = (Button) rootView.findViewById(R.id.btnLineOut);
+        btnUnassisted = (Button) rootView.findViewById(R.id.btnUnassisted);
+        btnSacFly = (Button) rootView.findViewById(R.id.btnSacFly);
+        btnSacBunt = (Button) rootView.findViewById(R.id.btnSacBunt);
+        btnDoublePlay = (Button) rootView.findViewById(R.id.btnDoublePlay);
+        btnTriplePlay = (Button) rootView.findViewById(R.id.btnTriplePlay);
+
+        initializeOnTouchListeners();
+        initializeOnClickListeners();
+    }
+
+    private void initializeOnClickListeners() {
+        btnFoulOut.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnFoulOutOnClick();
+                    }
+                }
+        );
+        btnFlyOut.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnFlyOutOnClick();
+                    }
+                }
+        );
+        btnSwinging.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnSwingingOnClick();
+                    }
+                }
+        );
+        btnLooking.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnLookingOnClick();
+                    }
+                }
+        );
+        btnGroundOut.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnGroundOutOnClick();
+                    }
+                }
+        );
+        btnLineOut.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnLineOutOnClick();
+                    }
+                }
+        );
+        btnUnassisted.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnUnassistedOnClick();
+                    }
+                }
+        );
+        btnSacFly.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnSacFlyOnClick();
+                    }
+                }
+        );
+        btnSacBunt.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnSacBuntOnClick();
+                    }
+                }
+        );
+        btnDoublePlay.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnDoublePlayOnClick();
+                    }
+                }
+        );
+        btnTriplePlay.setOnClickListener(
+                new View.OnClickListener() {
+                    public void onClick(View v) {
+                        btnTriplePlayOnClick();
+                    }
+                }
+        );
+    }
+
+    private void initializeOnTouchListeners() {
+        btnFoulOut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnFoulOutOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnFoulOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnFlyOut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnFlyOutOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnFlyOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnSwinging.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnSwingingOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnSwinging.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnLooking.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnLookingOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnLooking.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnGroundOut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnGroundOutOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnGroundOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnLineOut.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnLineOutOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnLineOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnUnassisted.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnUnassistedOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnUnassisted.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnSacFly.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnSacFlyOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnSacFly.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnSacBunt.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnSacBuntOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnSacBunt.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnDoublePlay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnDoublePlayOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnDoublePlay.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+        btnTriplePlay.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN)
+                    btnTriplePlayOnTouch();
+                else if (event.getAction() == MotionEvent.ACTION_UP)
+                    btnTriplePlay.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded));
+                return false;
+            }
+        });
+    }
+
+    private void btnFoulOutOnClick() {
+
+    }
+
+    private void btnFoulOutOnTouch() {
+        btnFoulOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnFlyOutOnClick() {
+
+    }
+
+    private void btnFlyOutOnTouch() {
+        btnFlyOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnSwingingOnClick() {
+
+    }
+
+    private void btnSwingingOnTouch() {
+        btnSwinging.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnLookingOnClick() {
+
+    }
+
+    private void btnLookingOnTouch() {
+        btnLooking.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnGroundOutOnClick() {
+
+    }
+
+    private void btnGroundOutOnTouch() {
+        btnGroundOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnLineOutOnClick() {
+
+    }
+
+    private void btnLineOutOnTouch() {
+        btnLineOut.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnUnassistedOnClick() {
+
+    }
+
+    private void btnUnassistedOnTouch() {
+        btnUnassisted.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnSacFlyOnClick() {
+
+    }
+
+    private void btnSacFlyOnTouch() {
+        btnSacFly.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnSacBuntOnClick() {
+
+    }
+
+    private void btnSacBuntOnTouch() {
+        btnSacBunt.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnDoublePlayOnClick() {
+
+    }
+
+    private void btnDoublePlayOnTouch() {
+        btnDoublePlay.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
+    private void btnTriplePlayOnClick() {
+
+    }
+
+    private void btnTriplePlayOnTouch() {
+        btnTriplePlay.setBackground(ContextCompat.getDrawable(getContext(), R.drawable.black_rounded_pressed));
+    }
+
     public interface OnFragmentInteractionListener {
 
     }
