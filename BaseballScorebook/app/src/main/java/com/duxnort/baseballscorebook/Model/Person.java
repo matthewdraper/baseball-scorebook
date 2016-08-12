df1592d78da51d508b26275d418e6ccdfb566838
Matthew Draper <matt_draper@outlook.com>
2016-08-12 11:59:16 -0500

    Outlined OverallStats, Player, Person, Umpire, and Team. Added State.

--- BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/Model/Person.java
+++ BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/Model/Person.java
@@ -3,5 +3,31 @@ package com.duxnort.baseballscorebook.Model;
 /**
  * Created by matt on 8/12/16.
  */
-public class Person {
+public abstract class Person {
+    private String firstName = null;
+    private String lastName = null;
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
