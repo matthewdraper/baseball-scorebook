d9009f8debc2f65d980dfce6e88727632b740d3b
Matthew Draper <matt_draper@outlook.com>
2016-08-12 16:10:37 -0500

    Pre-merge commit.

--- BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/Model/Game.java
+++ BaseballScorebook/app/src/main/java/com/duxnort/baseballscorebook/Model/Game.java
@@ -14,4 +14,103 @@ public class Game {
     private Date endTime = null;
     private ArrayList<Umpire> umpireList = null;
     private String fieldName = null;
+
+    public Game() {
+    }
+
+    public Game(ArrayList<GameState> gameStateList, ArrayList<LineupState> lineupStatesList,
+                int currGameStateIndex, int currLineupStateIndex, Team homeTeam, Team awayTeam,
+                Date startTime, Date endTime, ArrayList<Umpire> umpireList, String fieldName) {
+        this.gameStateList = gameStateList;
+        this.lineupStatesList = lineupStatesList;
+        this.currGameStateIndex = currGameStateIndex;
+        this.currLineupStateIndex = currLineupStateIndex;
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.umpireList = umpireList;
+        this.fieldName = fieldName;
+    }
+
+    public ArrayList<GameState> getGameStateList() {
+        return gameStateList;
+    }
+
+    public void setGameStateList(ArrayList<GameState> gameStateList) {
+        this.gameStateList = gameStateList;
+    }
+
+    public ArrayList<LineupState> getLineupStatesList() {
+        return lineupStatesList;
+    }
+
+    public void setLineupStatesList(ArrayList<LineupState> lineupStatesList) {
+        this.lineupStatesList = lineupStatesList;
+    }
+
+    public int getCurrGameStateIndex() {
+        return currGameStateIndex;
+    }
+
+    public void setCurrGameStateIndex(int currGameStateIndex) {
+        this.currGameStateIndex = currGameStateIndex;
+    }
+
+    public int getCurrLineupStateIndex() {
+        return currLineupStateIndex;
+    }
+
+    public void setCurrLineupStateIndex(int currLineupStateIndex) {
+        this.currLineupStateIndex = currLineupStateIndex;
+    }
+
+    public Team getHomeTeam() {
+        return homeTeam;
+    }
+
+    public void setHomeTeam(Team homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    public Team getAwayTeam() {
+        return awayTeam;
+    }
+
+    public void setAwayTeam(Team awayTeam) {
+        this.awayTeam = awayTeam;
+    }
+
+    public Date getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Date startTime) {
+        this.startTime = startTime;
+    }
+
+    public Date getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Date endTime) {
+        this.endTime = endTime;
+    }
+
+    public ArrayList<Umpire> getUmpireList() {
+        return umpireList;
+    }
+
+    public void setUmpireList(ArrayList<Umpire> umpireList) {
+        this.umpireList = umpireList;
+    }
+
+    public String getFieldName() {
+        return fieldName;
+    }
+
+    public void setFieldName(String fieldName) {
+        this.fieldName = fieldName;
+    }
+
 }
